a9142099eaf133c103360a0b9c206b3a0496e75b
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-07-21 04:44:38 +0000

    wip: [careers] Export structured data for Google Jobs.
    Fixes gitpod-io/website#696

--- src/components/careers/ExpandableJob.tsx
+++ src/components/careers/ExpandableJob.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import { colors, borders } from '../../styles/variables'
 import Arrow from '../Arrow'
+import TypeFoxLogo from '../../resources/typefox.png'
 
 const StyledExpandableJob = styled.div`
     position: relative;
@@ -76,6 +77,52 @@ const ExpandableJob = ({ title, intro, paragraphs, lists, textAfterTheLists, ren
         setIsRendered(!isRendered)
     }
 
+    const applicationEmail = `mailto:contact@typefox.io?subject=Application as ${title}`
+
+    const structuredData = {
+          "@context": "https://schema.org/",
+          "@type": "JobPosting",
+          "title": title,
+          "description": `<p>${intro}</p>${paragraphs.split('\n').map((p, i) => <p key={i+title}>{p}</p>)}`,
+          "hiringOrganization": {
+            "@type": "Organization",
+            "name": "TypeFox GmbH",
+            "sameAs": "https://www.typefox.io/",
+            "logo": TypeFoxLogo,
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "url": "https://www.typefox.io/contact/"
+            }
+          },
+          // Location: Kiel Office / Germany Remote / France Remote.
+          // Ref: https://developers.google.com/search/docs/data-types/job-posting#remote-jobs
+          "jobLocation": {
+            "@type": "Place",
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "Am Germaniahafen 1",
+              "addressLocality": "Kiel",
+              "postalCode": "24143",
+              "addressCountry": "Germany"
+            }
+          },
+          "applicantLocationRequirements": [{
+              "@type": "Country",
+              "name": "Germany"
+          },{
+              "@type": "Country",
+              "name": "France"
+          }],
+          "jobLocationType": "TELECOMMUTE",
+          "employmentType": "FULL_TIME",
+          "applicationContact": {
+            "@type": "ContactPoint",
+            "email": applicationEmail
+          },
+        };
+
+
+
     return (
         <StyledExpandableJob>
             <button onClick={toggleIsRendered}>
@@ -90,14 +137,14 @@ const ExpandableJob = ({ title, intro, paragraphs, lists, textAfterTheLists, ren
                 { lists.map((item, i) => <List key={i + item.title} {...item} />) }
                 <p className="after">{textAfterTheLists}</p>
                 <a 
-                    href={`mailto:contact@typefox.io?subject=Application as ${title}`}
+                    href={applicationEmail}
                     target="_blank"
                     className="btn btn--cta"
                 >
                     Apply Now
                 </a>
             </>) : null}
-            
+            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />;
         </StyledExpandableJob>
     )
 }
